0b83417c2da18d6f66116b5dd51f3d9fbb2979c0
jrme421 <jeromepchallet@gmail.com>
2024-02-13 06:32:42 +0900

    intro

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('.close-modal');
+//when using query selectors to select elements,
+//if there are many of them, only the 1st one gets selected
+// const btnsOpenModal = document.querySelector('.show-modal');
+// console.log(btnsOpenModal);
+//this query selector selects them all
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+for (leti = 0; i < btnsOpenModal.length; i++) {
+  //console.log(btnsOpenModal(i).textContent);
+  console.log(btnsOpenModal(i).textContent);
+}
